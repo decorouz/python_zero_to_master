3db06ac0101e81887695443061cb4ff127bd7314
Biola <abiola2326@gmail.com>
2021-04-13 02:32:13 +0100

    python email sender with smtplib

---
+++ email-playground/email_sender.py
@@ -0,0 +1,31 @@
+from email.message import EmailMessage
+import smtplib
+import ssl
+
+
+smtp_server = 'smtp.gmail.com'
+port = 587  # For starttls
+sender_email = 'decorouzweb@gmail.com'
+receiver_email = 'abiola2326@gmail.com'
+password = input("Type your password and press enter: ")
+message = ' Testing new approaches to use the EmailMessage module'
+
+msg = EmailMessage()
+msg.set_content(message)
+
+msg['Subject'] = 'Testing without ehlo'
+msg['From'] = sender_email
+msg['To'] = receiver_email
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+try:
+    server = smtplib.SMTP(smtp_server, port)
+    server.starttls(context=context)
+    server.login(sender_email, password)
+    server.send_message(msg)
+except Exception as e:
+    print(e)
+finally:
+    server.quit()
