80decedc9489f5cfc214d8da711d6e50f044e392
Biola <abiola2326@gmail.com>
2021-04-30 15:12:08 +0100

    Read pass from file

--- passwordchecker/checkmypass.py
+++ passwordchecker/checkmypass.py
@@ -9,7 +9,8 @@ def request_api_data(query_char):
 
     res = requests.get(url)
     if res.status_code != 200:
-        raise RuntimeError(f'Error fetching: {res.status_code}')
+        raise RuntimeError(
+            f'Error fetching: {res.status_code}, check the Api and Try again')
     return res
 
 
@@ -17,6 +18,7 @@ def get_password_leak_count(hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
 
     for h, count in hashes:
+
         if h == hash_to_check:
             return count
     return 0
@@ -29,16 +31,19 @@ def pwned_api_check(password):
     return get_password_leak_count(response, tail)
 
 
-def main(args):
-    for password in args:
+def main():
+    with open(sys.argv[1]) as f:
+        content = f.readlines()
+
+    for password in content:
         count = pwned_api_check(password)
         if count:
             print(
-                f'{password} was found {count} timmes.. you should change your password')
+                f'{password} was found {count} times... you should change your password')
         else:
             print(f'{password} was NOT found.')
     return 'done'
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+    sys.exit(main())
